a678408ecff540fc281b4d92e9bec57eb32474d7
toshilok <lapinig_carlo@yahoo.com>
2016-04-18 17:36:04 +0800

    First TodoApp

---
+++ todo/src/components/TodoApp.js
@@ -0,0 +1,26 @@
+/**
+ * Created by 0614-3625-cect on 4/18/2016.
+ */
+/**
+ * Created by albertoclarit on 4/18/16.
+ */
+import React from 'react';
+
+export default  class TodoApp extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+
+        return (
+            <div>
+            <center><h1>Welcome to ReactJS</h1></center>
+                <br>
+                  <h5>I Love Being IT!</h5>
+        </div>
+    )
+    }
+}
---
+++ todo/src/components/run.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 0614-3625-cect on 4/18/2016.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './TodoApp'
+
+
+ReactDOM.render(<TodoApp/>, document.getElementById('app'));
