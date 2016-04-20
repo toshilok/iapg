07f79cfb163f93eaa2f5d9ab618ae46108c88b5c
toshilok <lapinigcarlo@gmail.com>
2016-04-20 17:36:28 +0800

    Welcome Example

--- todo/src/components/TodoApp.js
+++ todo/src/components/TodoApp.js
@@ -10,17 +10,33 @@ export default  class TodoApp extends React.Component{
 
     constructor(props){
         super(props);
+        this.state={
+            name:'Student'
+        }
+        //this.onChange = this.onChange.bind(this);
     }
-
-
+    
+onChange(e){
+    this.setState({
+        name:e.target.value
+    })
+}
+ 
     render(){
 
         return (
+            
             <div>
-            <center><h1>Welcome to ReactJS</h1></center>
-                <br>
-                  <h5>I Love Being IT!</h5>
-        </div>
+            <center><h1>Welcome to ReactJS </h1>
+                  <h1>I Love Being IT!</h1>
+                  <font color="red"><h2>{this.state.name}</h2></font>
+                  <h2><strong>Enter Your Name:</strong></h2>
+                  <form>
+                  <input type="text" onChange={this.onChange.bind(this)}/>
+                  </form>
+                  </center>
+             </div>
+             
     )
     }
 }
